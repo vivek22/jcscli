07544446143fb081b19932593d9ebf437401f191
vivek sharma <vivek_s1990@yahoo.com>
2016-05-25 13:42:53 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,6 +6,6 @@ python cli_testing_dssANDhelp.py > dssANDhelp.log
 python cli_testing_vpc.py > vpc.log
 python cli_testing_compute.py > compute.log
 python cli_testing_iam.py > iam.log
-
+python cli_testing_rds.py > rds.log
 echo " complete "
 
