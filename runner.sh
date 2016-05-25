b3211c4a7d84cbab025e5840869c420da5a2ccab
vivek sharma <vivek_s1990@yahoo.com>
2016-05-25 13:47:28 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo " run "
+echo " running cli tests please wait !  "
 
 python cli_testing_dssANDhelp.py > dssANDhelp.log
 python cli_testing_vpc.py > vpc.log
 python cli_testing_compute.py > compute.log
 python cli_testing_iam.py > iam.log
 python cli_testing_rds.py > rds.log
+
 echo " complete "
 
