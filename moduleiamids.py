4fe6c4047721375c071084835f011be67d4f4aa8
vivek sharma <vivek_s1990@yahoo.com>
2016-05-25 13:46:19 +0530

    Update moduleiamids.py

--- moduleiamids.py
+++ moduleiamids.py
@@ -14,34 +14,4 @@ def get_accesskey_value(accesskeyjson):
         accesskey_value=data['credentials'][0]['blob']['access']
 	return accesskey_value
 
-def get_instancetype_id(instancetypejson):
-        data = json.loads(instancetypejson)
-        instancetype_id=data['DescribeInstanceTypesResponse']['instanceTypes']['item'][2]['id']
-	return instancetype_id
-    
-def get_keypair_id(keypairjson):
-        data = json.loads(keypairjson)
-        keypair_id=data['DescribeKeyPairsResponse']['keySet']['item'][0]['keyName']
-	return keyname_id
-
-def get_volume_id(volumejson):
-        data = json.loads(volumejson)
-        volume_id=data['DescribeVolumesResponse']['volumeSet']['item'][0]['volumeId']
-        return volume_id
-
-def get_sbsvolume_id(sbsvolumejson):
-        data = json.loads(sbsvolumejson)
-        sbsvolume_id=data['DescribeVolumesResponse']['volumeSet']['item'][0]['volumeId']
-        return sbsvolume_id
-
-def get_snapshot_id(snapshotjson):
-        data = json.loads(snapshotjson)
-        snapshot_id=data['DescribeSnapshotsResponse']['snapshotSet']['item']['snapshotId']
-        return snapshot_id
-
-def get_instance_id(instancejson):
-        data = json.loads(instancejson)
-        instance_id=data['DescribeInstancesResponse']['instancesSet']['item']['instanceId']
-        return instance_id
-
 
